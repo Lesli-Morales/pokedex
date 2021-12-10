b0b7e4481042a6fca7452d07d8704d7b7623c51d
Lesli Morales <gales031116@gmail.com>
2021-12-09 19:05:23 -0600

    Guardado asegurar

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,5 +1,61 @@
 const pokemonNombre = document.getElementById("pokemon")
 
+const inputBuscador= document.getElementById("heroBuscador")
+const btnBuscador = document.getElementById("heroBtn")
+
+btnBuscador.onclick = ()=>{
+    console.log("Pokemon: "+inputBuscador.value);
+
+    let nombrePokemon = inputBuscador.value
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`)
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then((pokemon)=>{
+      console.log(pokemon);
+      
+      let contenedorNombre= document.createElement("div")
+       
+      let idPokemon= document.createElement("label")
+      let imagen = document.createElement("img")
+      let nombre = document.createElement("p")
+
+     
+      contenedorNombre.classList.add("contenedorNombre")
+      idPokemon.classList.add("idPokemon")
+      imagen.classList.add("imagenPokemon")
+      nombre.classList.add("nombrePokemon")
+
+      
+      if(pokemon.id.toString().length>=1 && pokemon.id.toString().length<2){
+          idPokemon.innerText= "#00"+ pokemon.id
+      }
+      else if(pokemon.id.toString().length>=2 && pokemon.id.toString().length<3){
+          idPokemon.innerText= "#0"+ pokemon.id
+      }
+      else if(pokemon.id.toString().length>=3){
+          idPokemon.innerText= "#"+pokemon.id
+      }
+
+      imagen.src = pokemon.sprites.front_default;
+      nombre.innerText= pokemon.name;
+
+    
+      contenedorNombre.appendChild(imagen)
+      contenedorNombre.appendChild(nombre)
+      contenedorNombre.appendChild(idPokemon)
+      
+      pokemonNombre.appendChild(contenedorNombre)
+        
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+
+
+}
+
 function id(id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then(function(respuesta){
@@ -56,70 +112,3 @@ function infoPokemon(pokemon){
 
 infoPokemon(12)
 
-
-
-
-
-const inputBuscador= document.getElementById("heroBuscador")
-const btnBuscador = document.getElementById("heroBtn")
-
-btnBuscador.onclick = ()=>{
-    console.log("Pokemon: "+inputBuscador.value);
-
-    let nombrePokemon = inputBuscador.value
-
-    let contenedorNombre= document.createElement("div")
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`)
-    .then(function(respuesta){
-        return respuesta.json()
-    })
-    .then((pokemon)=>{
-      console.log(pokemon);
-      
-      let contenedorId = document.createElement("div");
-      let pokebola = document.createElement("img")
-     
-      let idPokemon= document.createElement("label")
-      let imagen = document.createElement("img")
-      let nombre = document.createElement("p")
-
-     
-      contenedorNombre.classList.add("contenedorNombre")
-      contenedorId.classList.add("contenedorId");
-      pokebola.classList.add("pokebola")
-      imagen.classList.add("imagenPokemon")
-
-      
-      if(pokemon.id.toString().length>=1 && pokemon.id.toString().length<2){
-          idPokemon.innerText= "#00"+ pokemon.id
-      }else if(pokemon.id.toString().length>=2 && pokemon.id.toString().length<3){
-          idPokemon.innerText= "#0"+ pokemon.id
-      }else if(pokemon.id.toString().length>=3){
-          idPokemon.innerText= pokemon.id
-      }
-
-      imagen.src = pokemon.sprites.front_default;
-      nombre.innerText= pokemon.name;
-      pokebola.src = "imagenes/pokebola.png"
-
-      contenedorId.appendChild(pokebola);
-      contenedorId.appendChild(idPokemon)
-      contenedorNombre.appendChild(contenedorId)
-
-      contenedorNombre.appendChild(imagen)
-      contenedorNombre.appendChild(nombre)
-
-      pokemonNombre.appendChild(contenedorNombre)
-
-
-
-
-        
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-
-
-}
